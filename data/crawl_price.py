0bff555b6235bd3b051329d75b929bcb0c50c533
curme <wiskyzhan@gmail.com>
2016-01-02 01:22:48 +0800

    update price data

--- data/crawl_price.py
+++ data/crawl_price.py
@@ -2,13 +2,16 @@ import time
 import requests
 
 RAW_LINK = "http://real-chart.finance.yahoo.com/table.csv?" + \
-           "s=%s&a=11&b=27&c=2014&d=11&e=28&f=2015&g=d&ignore=.csv"
+           "s=%s&a=11&b=27&c=2012&d=11&e=28&f=2015&g=d&ignore=.csv"
 STOCK_LIST = "stock_list.txt"
 
 def price_crawl(code):
 
     target_link =  RAW_LINK % code
-    response = requests.get(target_link)
+    try:    response = requests.get(target_link)
+    except: 
+        print code
+        return
     with file("%s.csv" % code, "wb") as f:
         f.write(response.content)
         print code, ".csv lines: ", len(response.content.split("\n"))
@@ -24,5 +27,5 @@ def get_stock_list(filepath):
 if __name__ == "__main__":
 
     stocks = get_stock_list(STOCK_LIST)
-    for stock in stocks:
+    for stock in stocks[62:]:
         price_crawl(stock)
